e6ee1d4d88ae01df07b10ca08425deec2eb10e27
Juliho Castillo <julihocc@users.noreply.github.com>
2024-03-14 18:02:36 -0600

    not passing jest test

--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,20 +1,26 @@
 // Put your checkEmotion and speak functions here! ✨
 // See ./original.js for their older JavaScript code.
 
+async function delay(ms) {
+	return new Promise(
+		setTimeout((resolve) => {
+			resolve;
+		}, ms)
+	);
+}
+
 async function checkEmotion(knownEmotions, emotion) {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve(knownEmotions.has(emotion));
-		}, 1000);
-	});
+	await delay(1000);
+	return knownEmotions.has(emotion);
 }
 
 async function speak(knownEmotions, newEmotion, phrase) {
 	const hasEmotion = await checkEmotion(knownEmotions, newEmotion);
 	if (hasEmotion) {
 		return `"${phrase}" (${newEmotion})`;
+	} else {
+		throw new Error(`Does not compute. I do not understand ${newEmotion}.`);
 	}
-	throw new Error(`Does not compute. I do not understand ${newEmotion}.`);
 }
 
 module.exports.checkEmotion = checkEmotion;
